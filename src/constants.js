fca1210e0ac8590f477af60b9cb67f24d080e7fc
Igor Gryp <igor.gryp@gmail.com>
2023-07-25 01:46:40 +0200

    changed url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,25 @@
-export const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
-export const metric_units = 'metric'
-export const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-export const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+export const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+export const metric_units = 'metric';
+export const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+export const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
