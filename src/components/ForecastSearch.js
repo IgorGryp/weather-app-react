1c0f5b41ba44a8b4c4745d398b0113d7b2227de5
IgorGryp <igor.gryp@gmail.com>
2023-10-03 10:55:51 +0200

    .

--- src/components/ForecastSearch.js
+++ src/components/ForecastSearch.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { API_KEY, metric_units } from '../constants';
 
 function ForecastSearch({ locationId, setForecastList }) {
